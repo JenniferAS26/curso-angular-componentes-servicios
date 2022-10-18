557637ecd7e30a101f05dccd3b707700d890e7c7
JenniferAS26 <90392630+JenniferAS26@users.noreply.github.com>
2022-10-18 12:15:32 -0500

    Shopping card and services

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-
-import { Product } from './models/product.model';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,32 +8,6 @@ export class AppComponent {
   imgParent = '';
   // https://www.w3schools.com/howto/img_avatar2.png
   showImg = true;
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'EL mejor juguete',
-      price: 565,
-      image: './assets/images/toy.jpg'
-    },
-    {
-      id: '2',
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: './assets/images/bike.jpg'
-    },
-    {
-      id: '3',
-      name: 'Colleción de albumnes',
-      price: 34,
-      image: './assets/images/album.jpg'
-    },
-    {
-      id: '4',
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/images/books.jpg'
-    },
-  ];
 
   onLoaded(img: string) {
     console.log('Log padre', img);
