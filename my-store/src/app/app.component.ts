a700db776a30c03ea5140126cad187d0bdfd1f12
JenniferAS26 <90392630+JenniferAS26@users.noreply.github.com>
2022-10-25 15:57:05 -0500

    Authorization: token and creation of interceptors

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+
+import { AuthService } from './services/auth.service';
+import { UsersService } from './services/users.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -6,8 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   imgParent = '';
-  // https://www.w3schools.com/howto/img_avatar2.png
   showImg = true;
+  token = '';
+
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService
+  ) {  }
 
   onLoaded(img: string) {
     console.log('Log padre', img);
@@ -16,4 +24,29 @@ export class AppComponent {
   toggleImg() {
     this.showImg = !this.showImg;
   }
+
+  createUser() {
+    this.usersService.create({
+      name: 'Jenn',
+      email: 'jenn@mail.com',
+      password: '1234'
+    })
+    .subscribe(rta => {
+      console.log(rta);
+    });
+  }
+
+  // login() {
+  //   this.authService.login('jenn@mail.com', '1234')
+  //   .subscribe(rta => {
+  //     this.token = rta.access_token;
+  //   });
+  // }
+
+  // getProfile() {
+  //   this.authService.profile(this.token)
+  //   .subscribe(profile => {
+  //     console.log(profile);
+  //   });
+  // }
 }
--- my-store/src/app/services/auth.service.ts
+++ my-store/src/app/services/auth.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+import { Auth } from '../models/auth.model';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private apiUrl = `${environment.API_URL}/api/auth`;
+
+  constructor(
+    private http: HttpClient // inyeccion de dependencias
+  ) { }
+
+  login(email: string, password: string) {
+    return this.http.post<Auth>(`${this.apiUrl}/login`, {email, password});
+  }
+
+  profile(token: string) {
+    /*const headers = new HttpHeaders();
+    headers.set('Authorization', `Bearer ${token}`); // dinamicos y solo retorna el headers*/
+    return this.http.get<User>(`${this.apiUrl}/profile`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        // 'Content-type': 'application/json'
+      }
+    });
+  }
 }
